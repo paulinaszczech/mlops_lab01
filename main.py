1e0eeb21e0665b37bb9da9a38077da93364ba977
Paulina <paulina.szczech89@gmail.com>
2025-04-13 12:57:11 +0200

    add main

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from mlops-lab01!")
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    env_file = f".env.{environment}"
+    if not os.path.exists(env_file):
+        raise FileNotFoundError(f"Plik {env_file} nie istnieje.")
+    load_dotenv(env_file)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Wczytaj zmienne środowiskowe z odpowiedniego pliku .env."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="Środowisko do wczytania (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
