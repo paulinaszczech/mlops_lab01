28688a54c08f98a4095538e5dbd4734388755d0b
Paulina <paulina.szczech89@gmail.com>
2025-05-01 22:20:46 +0200

    new

--- inference.py
+++ inference.py
@@ -1,7 +1,9 @@
 import joblib
 
+
 def load_model(filename="model.joblib"):
     return joblib.load(filename)
+
 
 def predict(model, input_data):
     prediction = model.predict([input_data])
--- training.py
+++ training.py
@@ -6,13 +6,16 @@ import joblib
 def load_data():
     return load_iris()
 
+
 def train_model(data):
     model = LogisticRegression(max_iter=1000)
     model.fit(data.data, data.target)
     return model
 
+
 def save_model(model, filename="model.joblib"):
     joblib.dump(model, filename)
+
 
 if __name__ == "__main__":
     data = load_data()
